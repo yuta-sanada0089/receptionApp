ad49489345b8387a8ad9749f9a8e497c54794f5b
yuta sanada <sndyut0089@gmail.com>
2021-07-04 02:16:24 +0900

    Initial Commit

---
+++ entranceApp/entranceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  entranceAppApp.swift
+//  entranceApp
+//
+//  Created by yuta sanada on 2021/07/04.
+//
+
+import SwiftUI
+
+@main
+struct entranceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
