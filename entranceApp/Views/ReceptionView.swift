386dd1d40f6655b6080f61fc519a23ef3906c086
yuta sanada <sndyut0089@gmail.com>
2021-07-12 02:38:25 +0900

    :+1: implements views

---
+++ entranceApp/Views/ReceptionView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ReceptionView: View {
+    var body: some View {
+        Text("Reception View")
+    }
+}
+
+struct ReceptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReceptionView()
+    }
+}
---
+++ entranceApp/Views/SettingView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text("Setting")
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
