d83d84cdc59ae0bbe494886a7c7eb137ee7a110f
yuta sanada <sndyut0089@gmail.com>
2021-07-25 00:37:32 +0900

    :shower: appName

--- receptionApp/receptionAppApp.swift
+++ receptionApp/receptionAppApp.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 @main
-struct entranceAppApp: App {
+struct receptionAppApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
