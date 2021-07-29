3aa6b85ce83d0a3d02c3af03172fb0dc4e4827f5
yuta sanada <sndyut0089@gmail.com>
2021-07-29 13:13:58 +0900

    :+1: implement button view

--- receptionApp/Views/ReceptionView.swift
+++ receptionApp/Views/ReceptionView.swift
@@ -1,8 +1,36 @@
 import SwiftUI
 
 struct ReceptionView: View {
+    private var columns: [GridItem] = Array(repeating: .init(.flexible(maximum: 364), spacing: CGFloat(10.0) ), count: 3)
     var body: some View {
-        Text("Reception View")
+        LazyVGrid(columns: columns, alignment: .center, spacing: 10, content: {
+            ForEach((1...5), id: \.self) {num in
+                ReceptionButtonView(title: "担当者で探す方はこちら", subTitle: "interview")
+            }
+        })
+    }
+}
+
+struct ReceptionButtonView: View {
+    var title: String
+    var subTitle: String
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.defaultFont(ofSize: 24))
+            Divider()
+                .padding(.horizontal, 16)
+                .background(Color.borderColor)
+            Text(subTitle)
+                .padding(8)
+                .font(.defaultFont(ofSize: 14))
+            Rectangle()
+                .foregroundColor(.linkTextColor)
+                .frame(height: 3)
+        }
+        .frame(height: 163)
+        .border(Color.borderColor)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
 
