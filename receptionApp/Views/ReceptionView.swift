ea16d84f5ad885e7dfc0e697472a562f4759793f
yuta sanada <sndyut0089@gmail.com>
2021-07-31 06:05:11 +0900

    :+1: update view

--- receptionApp/Views/ReceptionView.swift
+++ receptionApp/Views/ReceptionView.swift
@@ -3,11 +3,29 @@ import SwiftUI
 struct ReceptionView: View {
     private var columns: [GridItem] = Array(repeating: .init(.flexible(maximum: 364), spacing: CGFloat(10.0) ), count: 3)
     var body: some View {
-        LazyVGrid(columns: columns, alignment: .center, spacing: 10, content: {
-            ForEach((1...5), id: \.self) {num in
-                ReceptionButtonView(title: "担当者で探す方はこちら", subTitle: "interview")
-            }
-        })
+        VStack {
+            Spacer().frame(height: 70)
+            Image("default_logo")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 730, maxHeight: 190)
+            Spacer()
+                .frame(height: 16)
+            Text("ご来訪いただきありがとうございます。\n以下のボタンより入館手続きをお願いします")
+                .frame(height: 64)
+                .font(.defaultFont(ofSize: 18))
+                .multilineTextAlignment(.center)
+            Spacer()
+                .frame(height: 16)
+            LazyVGrid(columns: columns, alignment: .center, spacing: 10, content: {
+                ForEach((1...5), id: \.self) {num in
+                    ReceptionButtonView(title: "担当者で探す方はこちら", subTitle: "interview")
+                }
+            })
+            Spacer()
+        }
+        .hideNavigationBar()
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
@@ -18,19 +36,22 @@ struct ReceptionButtonView: View {
         VStack {
             Text(title)
                 .font(.defaultFont(ofSize: 24))
+                .padding(8)
             Divider()
-                .padding(.horizontal, 16)
                 .background(Color.borderColor)
             Text(subTitle)
-                .padding(8)
                 .font(.defaultFont(ofSize: 14))
+                .padding(8)
             Rectangle()
                 .foregroundColor(.linkTextColor)
                 .frame(height: 3)
         }
-        .frame(height: 163)
-        .border(Color.borderColor)
+        .padding(.horizontal, 30)
+        .frame(height: 162)
+        .background(Color.light100)
         .clipShape(RoundedRectangle(cornerRadius: 10))
+        .compositingGroup()
+        .shadow(color: .dark26, radius: 4, x: 0, y: 0)
     }
 }
 
