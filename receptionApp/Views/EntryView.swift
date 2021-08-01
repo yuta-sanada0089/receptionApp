d4bb2997fc713e3b68c1a84f91700a200b70bc1b
yuta sanada <sndyut0089@gmail.com>
2021-08-02 06:26:22 +0900

    :+1: add view

---
+++ receptionApp/Views/EntryView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct EntryView: View {
+    var body: some View {
+        Text("Entry")
+    }
+}
+
+struct EntryView_Previews: PreviewProvider {
+    static var previews: some View {
+        EntryView()
+    }
+}
