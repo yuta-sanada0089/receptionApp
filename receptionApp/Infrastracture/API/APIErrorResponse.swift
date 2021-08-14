4a34563f2af486580be266f260f9cfd0bfd805fb
yuta sanada <sndyut0089@gmail.com>
2021-08-15 06:03:54 +0900

    :recycle: refactor APIErrorResponse

--- receptionApp/Infrastracture/API/APIErrorResponse.swift
+++ receptionApp/Infrastracture/API/APIErrorResponse.swift
@@ -1,11 +1,17 @@
-import Foundation
+import ObjectMapper
 
-struct APIErrorResponse: Decodable {
+class APIErrorResponse: ImmutableMappable {
     let title: String
     let errors: String
     let status: Int
 
     var message: String {
         errors
+    }
+    
+    required init(map: Map) throws {
+        title = try map.value("title")
+        errors = try map.value("errors")
+        status = try map.value("status")
     }
 }
