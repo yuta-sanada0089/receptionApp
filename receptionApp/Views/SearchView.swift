3a037bb5b9f19ddd8d474f9cac3a151d7d7b679f
yuta sanada <sndyut0089@gmail.com>
2021-08-24 23:49:46 +0900

    :+1: implements SearchView

--- receptionApp/Views/SearchView.swift
+++ receptionApp/Views/SearchView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct SearchView: View {
+    @ObservedObject private var viewModel = SearchViewModel()
     var body: some View {
         ScrollView {
             VStack {
@@ -16,36 +17,15 @@ struct SearchView: View {
                 Spacer()
                     .frame(height: 48)
                 LazyVGrid(columns: Array(repeating: GridItem(), count: 3), spacing: 10.0, content: {
-                    ForEach(0..<30) { index in
-                        HStack(spacing: 8.0) {
-                            Image("ic_person")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 88, height: 88)
-                                .background(Color.dark12)
-                                .cornerRadius(44)
-                            VStack(alignment: .leading, spacing: 8.0) {
-                                Text("エンジニア")
-                                    .font(.defaultFont(ofSize: 14))
-                                    .padding(4)
-                                    .background(Color.dark12)
-                                Text("真田　雄太")
-                                    .font(.defaultFont(ofSize: 24))
-                                Text("さなだ　ゆうた")
-                                    .font(.defaultFont(ofSize: 14))
-                            }
-                            Spacer()
-                        }
-                        .padding(.horizontal, 10)
-                        .frame(width: 308, height: 133)
-                        .background(Color.light100)
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                        .compositingGroup()
-                        .shadow(color: .dark26, radius: 4, x: 0, y: 0)
+                    ForEach(viewModel.members, id: \.id) { user in
+                        UserCardView(user: user)
                     }
-                }).padding(.trailing)
+                })
+                .padding(.leading, 125)
+                .padding(.trailing, 125)
             }
         }
+        .hideNavigationBar()
     }
 }
 
