ddbaeb5f0335a811ff39502d9e3f9d2c953178f6
yuta sanada <sndyut0089@gmail.com>
2021-08-24 23:45:51 +0900

    :+1: update model

--- receptionApp/Domain/Models/User.swift
+++ receptionApp/Domain/Models/User.swift
@@ -7,10 +7,32 @@ class User: ImmutableMappable {
     let name: String
     let deleted: Bool
     let realName: String
-    let profile: UserProfile
+    let profile: Profile
     let isAdmin: Bool
     let isOwner: Bool
     let isPrimaryOwner: Bool
+    
+    init(
+        id: String,
+        teamId: String,
+        name: String,
+        deleted: Bool,
+        realName: String,
+        profile: Profile,
+        isAdmin: Bool,
+        isOwner: Bool,
+        isPrimaryOwner: Bool
+    ) {
+        self.id = id
+        self.teamId = teamId
+        self.name = name
+        self.deleted = deleted
+        self.realName = realName
+        self.profile = profile
+        self.isAdmin = isAdmin
+        self.isOwner = isOwner
+        self.isPrimaryOwner = isPrimaryOwner
+    }
     
     required init(map: Map) throws {
         id = try map.value("id")
@@ -18,7 +40,7 @@ class User: ImmutableMappable {
         name = try map.value("name")
         deleted = try map.value("deleted")
         realName = try map.value("real_name")
-        profile = try map.value("profiie")
+        profile = try map.value("profile")
         isAdmin = try map.value("is_admin")
         isOwner = try map.value("is_owner")
         isPrimaryOwner = try map.value("is_primary_owner")
