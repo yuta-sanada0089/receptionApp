942b76781e776055f9ef75eac70fded0be06202b
yuta sanada <sndyut0089@gmail.com>
2021-09-02 02:59:10 +0900

    :+1: initialize .env

--- receptionApp/receptionAppApp.swift
+++ receptionApp/receptionAppApp.swift
@@ -12,6 +12,30 @@ struct receptionAppApp: App {
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        setupNabigationBar()
+        
+        guard let path = Bundle.main.path(forResource: ".env", ofType: nil) else {
+            fatalError("Not found: '/path/to/.env'.\nPlease create .env file reference from .env.sample")
+        }
+        let url = URL(fileURLWithPath: path)
+        do {
+            let data = try Data(contentsOf: url)
+            let str = String(data: data, encoding: .utf8) ?? "Empty File"
+            let clean = str.replacingOccurrences(of: "\"", with: "").replacingOccurrences(of: "'", with: "")
+            let envVars = clean.components(separatedBy:"\n")
+            for envVar in envVars {
+                let keyVal = envVar.components(separatedBy:"=")
+                if keyVal.count == 2 {
+                    setenv(keyVal[0], keyVal[1], 1)
+                }
+            }
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+        return true
+    }
+    
+    private func setupNabigationBar() {
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
         appearance.backgroundColor = .clear
@@ -20,6 +44,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UINavigationBar.appearance().standardAppearance = appearance
         UINavigationBar.appearance().scrollEdgeAppearance = appearance
         UINavigationBar.appearance().compactAppearance = appearance
-        return true
     }
 }
