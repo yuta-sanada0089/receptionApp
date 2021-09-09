bc3da2b60b1517e0a68f1761cff120f268829337
yuta sanada <sndyut0089@gmail.com>
2021-09-09 22:13:08 +0900

    :+1: add textFieldBorder

--- receptionApp/Extension/ViewExtension.swift
+++ receptionApp/Extension/ViewExtension.swift
@@ -4,4 +4,16 @@ extension View {
     func hideNavigationBar() -> some View {
         modifier(HideNavigationBarModifier())
     }
+    
+    func textFieldBorder(colors: [Color]) -> some View {
+        return RoundedRectangle(cornerRadius: 10)
+            .strokeBorder(
+                LinearGradient(
+                    gradient: .init(colors: colors),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                ),
+                lineWidth: 1
+            )
+    }
 }
