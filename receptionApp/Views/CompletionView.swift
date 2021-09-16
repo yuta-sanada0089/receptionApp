7b3d889bc8d073d93617899670a4bc66f7d6e51f
yuta sanada <sndyut0089@gmail.com>
2021-09-16 15:43:18 +0900

    :+1: implement view

--- receptionApp/Views/CompletionView.swift
+++ receptionApp/Views/CompletionView.swift
@@ -1,20 +1,45 @@
 import SwiftUI
 
 struct CompletionView: View {
-    @Environment(\.presentationMode) var presentationMode
-    
-    var body: some View {
-        NavigationLink(destination: ContentView(), label: {
-            Text("next view")
-        })
-        Button("画面を閉じる") {
-            self.presentationMode.wrappedValue.dismiss()
-        }
-    }
-}
+    @State private var count = 10
+    @EnvironmentObject var envData: EnvironmentViewModel
+    let user: User?
 
-struct CompletionView_Previews: PreviewProvider {
-    static var previews: some View {
-        CompletionView()
+    var body: some View {
+        VStack(alignment: .center, spacing: 20) {
+            Text("呼び出し完了")
+                .font(.defaultFont(ofSize: 28))
+                .padding()
+            iconImageView(user: user)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .background(Color.dark12)
+                .cornerRadius(100)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 100)
+                        .stroke(Color.linkTextColor, lineWidth: 10)
+                )
+                .padding()
+            Text("呼び出しが完了しました。しばらくお待ちください。\n間もなく担当者が参ります。")
+                .multilineTextAlignment(.center)
+                .padding()
+            Text("\(count)秒後にTOPに戻ります。")
+        }
+        .onAppear() {
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+                count -= 1
+                if count == 0 {
+                    timer.invalidate()
+                    envData.isNavigationActive.wrappedValue = false
+                }
+            }
+        }
+        .hideNavigationBar()
+    }
+    
+    private func iconImageView(user: User?) -> Image {
+        guard let user = user else { return Image("ic_icon") }
+        return Image(uiImage: user.profile.displayImage)
     }
 }
