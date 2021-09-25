c35c8e558dc60cf73503bc79483ee30336943465
yuta sanada <sndyut0089@gmail.com>
2021-09-26 04:18:33 +0900

    :+1: implement viewModel

---
+++ receptionApp/ViewModels/ReceptionConfirmViewModel.swift
@@ -0,0 +1,43 @@
+import Foundation
+import Combine
+
+class ReceptionConfirmViewModel: ObservableObject, DepecdsOnSlackChatPostMessageRepository {
+    @Published var isPushActive: Bool
+    @Published var error: Error?
+    @Published var user: User?
+    @Published var buttonType: ButtonType
+    @Published var companyName: String?
+    @Published var visitorName: String
+    @Published var guestCount: Int
+    let channelId = ProcessInfo.processInfo.environment["SLACK_CHANNEL_ID"]!
+    var cancellables = [AnyCancellable]()
+    
+    init(buttonType: ButtonType, user: User?, companyName: String?, visitorName: String, guestCount: Int) {
+        isPushActive = false
+        self.buttonType = buttonType
+        self.user = user
+        self.companyName = companyName
+        self.visitorName = visitorName
+        self.guestCount = guestCount
+    }
+    
+    func postSlack() {
+        slackChatPostMessageRepository
+            .create(parameter: ["channel": "C026XDHNL7P", "text": text()])
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished: break
+                case .failure(let error):
+                    self.error = error
+                }
+            }, receiveValue: { _ in
+                self.isPushActive = true
+            })
+            .store(in: &cancellables)
+    }
+    
+    private func text() -> String {
+        // <@メンバーID>
+        return "<!here>\n\(visitorName)が来られました"
+    }
+}
